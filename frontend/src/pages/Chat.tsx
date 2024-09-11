3cbee0bcc072fdff2c04f882d8c1173ce75090f5
Parikshit <parikshitgupta2001@gmail.com>
2024-09-11 16:30:06 +0530

    Add functionality to set username global variable

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -4,8 +4,45 @@ import Send from "../components/Send";
 import FriendList from "../components/FriendList";
 import StatusBar from "../components/StatusBar";
 import ChatBox from "../components/ChatBox";
+import { useCallback, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../state/store";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { setUserName } from "../state/username/usernameSlice";
+import { URL } from "../utils/constants";
 
 function Chat() {
+  const socket = useSelector((state: RootState) => state.socket.socket);
+  const username = useSelector((state: RootState) => state.username.username);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const func = useCallback(async () => {
+    await axios
+      .get(`${URL}/api/user/getusername`, { withCredentials: true })
+      .then((res) => {
+        const usernamevalue = res.data.username as string | undefined;
+        if (usernamevalue === undefined) {
+          navigate("/signin");
+        } else {
+          dispatch(setUserName(usernamevalue));
+        }
+      });
+  }, []);
+
+  useEffect(() => {
+    if (username === undefined) {
+      func().then(() => {
+        socket?.emit("set-username", username);
+      });
+    }
+    socket?.emit("set-username", username);
+    return () => {
+      socket?.emit("remove-username", username);
+    };
+  }, [socket, username, func]);
+  
   return (
     <div className="grid sm:grid-cols-12 h-screen">
       <div className="col-span-4 border-r grid grid-rows-12">
