4a6d855c083d8cac6a1346bf8292d1083f761db2
Parikshit <parikshitgupta2001@gmail.com>
2024-09-11 15:04:07 +0530

    Route getUsername request

--- backend/src/routes/User.ts
+++ backend/src/routes/User.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
-import { signinUser, signoutUser, signupUser } from "../controllers/User";
+import {
+  getUsername,
+  signinUser,
+  signoutUser,
+  signupUser,
+} from "../controllers/User";
+import { verifyJWT } from "../middlewares/auth";
 
 const router = Router();
 
 router.route("/signin").post(signinUser);
 router.route("/signup").post(signupUser);
 router.route("/signout").post(signoutUser);
-
+router.route("/getusername").get(verifyJWT, getUsername);
 export default router;
